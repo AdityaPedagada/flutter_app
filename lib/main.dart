1967d770639ceaff1c58aea9295f10d8deac821f
Aditya <adityaabsolin@gmail.com>
2021-08-19 16:29:11 +0530

    I am a box chagnes

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,24 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        child: Text("Wlecom to Flutter"),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          alignment: Alignment.center,
+          child: Text(
+            "I am a Box",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          decoration: BoxDecoration(
+              color: Colors.blueGrey,
+              gradient: LinearGradient(colors: [Colors.pink, Colors.yellow]),
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [BoxShadow(blurRadius: 5, color: Colors.grey)]),
+        ),
       ),
     );
   }
